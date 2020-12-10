9c36c586c14230a59f6f9b9469e5928a53f303a6
codecat15 <codecat15@gmail.com>
2020-12-09 18:44:28 -0600

    resolving the crash
    adding coding keys to the response model

--- URLSessionSample/ModalClass/EmployeeResponse.swift
+++ URLSessionSample/ModalClass/EmployeeResponse.swift
@@ -12,4 +12,16 @@ class EmployeeResponse : Decodable
     let employeeId, depid: Int
     let salary: Double
     let name, role, joining, workPhone: String
+
+
+    enum CodingKeys: String, CodingKey {
+        case employeeId = "id" // the api is retuning the key as id and you have employeeId here, so in such
+        // situations you can use coding keys, because they help you to do the mapping
+        // between the api response and your response model
+        case depid = "dep_id"
+        case name, role
+        case joining = "joining_date" // same goes over here, the response contains the key as joining_date
+        // and your model has the property as joining
+        case salary, workPhone
+    }
 }
